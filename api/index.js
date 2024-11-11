597c85853f294af2542430015bac5ff422915161
ngthhoangkim <nguyenthihoangkim07022004@gmail.com>
2024-11-11 07:37:49 +0700

    api trang yêu thích

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
+import favoriteRoute from "./routes/favorite.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
@@ -48,6 +49,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+app.use("/api/favorite", favoriteRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
